6df07bcb77b24a0ddd7ce8d4fd830f3b6d6e6ec2
guilalf <guilherme.projetoismart@gmail.com>
2022-06-23 18:57:48 -0300

    New Update

--- creator.bat
+++ creator.bat
@@ -1,4 +1,8 @@
 @echo off
 set cmnd=%1
+cls
 python "C:\workspace\python\cli\creatorp.py" %CD% %cmnd%
 pause
+cls
+echo -------------- Seu projeto foi Criado -----------------
+ls
--- creatorp.py
+++ creatorp.py
@@ -1,23 +1,26 @@
 import sys
 import os
-
-dire = 'newProject'
-html = 'index.html'
-css = 'style.css'
-js = 'script.js'
+# Project consts
+HTML = 'index.html'
+CSS = 'style.css'
+JS = 'script.js'
 temp = open('C:/workspace/python/cli/assets/template.html')
 
 entry = sys.argv[1::]
 
 def fronta() :
-    os.mkdir(dire)
-    arch = open('{path}/{n1}'.format(path = dire, n1 = html) ,'w')
-    temp1 = temp.read()
-    arch.write(temp1)
-    arch.close()
-    open('{path}/{n1}'.format(path = dire, n1 = css), 'w')
-    open('{path}/{n1}'.format(path = dire, n1 = js), 'w')
-
+    print('----------------- Criando seu projeto ------------------')
+    dire = input('Insira o nome do projeto: ')
+    if os.path.exists(dire) :
+        print('O projeto(newProject) já existe...')
+    else:
+        os.mkdir(dire)
+    with open(f'{dire}/{HTML}','w') as f_i:
+        temp1 = temp.read()
+        f_i.write(temp1)
+        f_i.close()
+        open(f'{dire}/{CSS}', 'w')
+        open(f'{dire}/{JS}', 'w')
 # Verifyer of flags
 if entry[1] == '--html' :
     fronta()
